59ed0677022463add3397dba844aa91e76d28f3b
Guilherme Lellis <lguilherme44@gmail.com>
2021-08-21 14:20:26 -0300

    icons no add food

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
-import { FiCheckSquare } from "react-icons/fi";
+import { FiCheckSquare, FiImage, FiDollarSign } from "react-icons/fi";
+import { MdDescription, MdTitle } from "react-icons/md";
 import { Form } from "./styles";
 import Modal from "../Modal";
 import Input from "../Input";
@@ -51,12 +52,16 @@ export default function ModalAddFood({
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
       <Form ref={formRef} onSubmit={handleSubmit}>
         <h1>Novo Prato</h1>
-        <Input name="image" placeholder="Cole o link aqui" />
+        <Input name="image" placeholder="Link da imagem" icon={FiImage} />
 
-        <Input name="name" placeholder="Ex: Moda Italiana" />
-        <Input name="price" placeholder="Ex: 19.90" />
+        <Input name="name" placeholder="Titulo" icon={MdTitle} />
+        <Input name="price" placeholder="Valor" icon={FiDollarSign} />
 
-        <Input name="description" placeholder="Descrição" />
+        <Input
+          name="description"
+          placeholder="Descrição"
+          icon={MdDescription}
+        />
         <button type="submit" data-testid="add-food-button">
           <p className="text">Adicionar Prato</p>
           <div className="icon">
